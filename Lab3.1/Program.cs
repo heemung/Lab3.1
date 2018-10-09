f71d9500a1f61a18bdcef41b1212283f570d6535
Clayton <clayton.cox@gmail.com>
2018-10-09 16:38:20 -0400

    Lab version 2.

--- Lab3.1/Program.cs
+++ Lab3.1/Program.cs
@@ -10,12 +10,91 @@ namespace Lab3._1
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            string userInput, userName;
+            int userNumber = 0;
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            Console.WriteLine("Whats Your Name?");
+            userName = Console.ReadLine();
+            //yes no loop
+            do
+            {
+                do
+                {
+                    userNumber = 0;
+                    Console.WriteLine("Enter a number 1 and 100: ");
+                    userInput = Console.ReadLine();
+
+                    //test user input
+                    if (userInput.Length <= 3)
+                    {
+                        userNumber = int.Parse(userInput);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Try again.");
+                    }
+
+                } while (userNumber <= 0 || userNumber > 100);
+
+                //run logic
+                InclusiveChecks(userNumber, userName);
+                Console.WriteLine("Do you want to run the program again. y/n ?");
+
+            } while (Console.ReadLine().ToLower() == "y");
+
+            Console.WriteLine("Goodbye. Press Any Key To Exit");
+            Console.ReadLine();
+        }
+
+        //logic for even or odd number
+        static string EvenTrue(int tempUserNumber)
+        {
+            tempUserNumber = tempUserNumber % 2;
+
+            if (tempUserNumber == 0)
+            {
+                return "even";
+            }
+            else
+            {
+                return "odd";
+            }
+        }
+
+        //logic and print for case
+        static void InclusiveChecks(int tempCheckNumber, string name)
+        {
+            if (EvenTrue(tempCheckNumber) == "odd")
+            {
+                if (tempCheckNumber > 60)
+                {
+                    Console.WriteLine("Hi {0}, {1} is {2} and greater than 60.", name,
+                        tempCheckNumber, EvenTrue(tempCheckNumber));
+                }
+                else
+                {
+                    Console.WriteLine("Hi {0}, {1} is {2}.", name,
+                        tempCheckNumber, EvenTrue(tempCheckNumber));
+                }
+            }
+
+            else if((tempCheckNumber >= 2 && tempCheckNumber <= 25) &&
+                (EvenTrue(tempCheckNumber) == "even"))
+            {
+                Console.WriteLine("Hi {0}, {1} is {2} and less than 25.", name,
+                    tempCheckNumber, EvenTrue(tempCheckNumber));
+            }
+
+            else if ((tempCheckNumber >= 26 && tempCheckNumber <= 60) &&
+                (EvenTrue(tempCheckNumber) == "even"))
+            {
+                Console.WriteLine("Hi {0}, {1} is {2} and 26-60.", name,
+                    tempCheckNumber, EvenTrue(tempCheckNumber));
+            }
+
+            else
+                Console.WriteLine("Hi {0}, {1} is {2} and greater 60.", name,
+                    tempCheckNumber, EvenTrue(tempCheckNumber));
         }
     }
 }
